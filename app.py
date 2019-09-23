973b74d5310c6907ba69d05c5d87ed5388828da6
Terkea <terkeabt@gmail.com>
2019-09-23 18:42:06 +0100

    flask interface template

--- app.py
+++ app.py
@@ -1,17 +1,15 @@
-from flask import Flask, escape, request
+from flask import Flask, escape, request, render_template
 import cv2
 import numpy
 import os
 import time
 import tools as tools
+
 app = Flask(__name__)
 
 @app.route('/')
-def hello():
-    name = request.args.get("name", "World")
-    print(tools.measure_temp())
-    print(tools.get_machine_storage())
-    return f'Hello, {escape(name)}!'
+def index():
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
