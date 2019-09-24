5e95110d6866320fde26468f5fe1b8dc270ebe3f
Terkea <terkeabt@gmail.com>
2019-09-24 20:13:40 +0100

    login form, half way done. VALIDATION DOESN'T WORK

--- securepi/__init__.py
+++ securepi/__init__.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from securepi import forms
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+app.config['SECRET_KEY'] = 'SEX BOT'
 db = SQLAlchemy(app)
 
 from securepi import routes
--- securepi/routes.py
+++ securepi/routes.py
@@ -1,5 +1,6 @@
-from flask import Flask, escape, request, render_template
+from flask import Flask, escape, request, render_template, redirect, url_for, flash
 from securepi import app, tools
+from securepi.forms import LoginForm
 
 TEMPERATURE = tools.measure_temp()
 MEMORY_AVAILABLE = tools.get_machine_storage()
@@ -8,6 +9,17 @@ MEMORY_AVAILABLE = tools.get_machine_storage()
 def index():
     return render_template('index.html', temperature_value = TEMPERATURE, memory_available_value = MEMORY_AVAILABLE)
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/login/', methods=['GET', 'POST'])
 def login():
-    return render_template('login.html')
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        email = str(form.email.data)
+        password = tools.encrypt(str(form.password.data))
+        print("email:  {}, password: {}".format(email, password))
+        print('SUCCESS')
+        return redirect(url_for('index'))
+    else:
+        flash('Input a valid account')
+        print("FAIL")
+    return render_template('login.html', form=form)
