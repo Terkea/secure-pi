111ede3029d6722e914bbc7cf8b715ff104c9be0
Terkea <terkeabt@gmail.com>
2019-09-24 21:20:05 +0100

    login functionality

--- securepi/routes.py
+++ securepi/routes.py
@@ -1,6 +1,7 @@
-from flask import Flask, escape, request, render_template, redirect, url_for, flash
+from flask import Flask, escape, request, render_template, redirect, url_for, session
 from securepi import app, tools
 from securepi.forms import LoginForm
+from securepi.models import User, Email, Picture, WhiteList
 
 TEMPERATURE = tools.measure_temp()
 MEMORY_AVAILABLE = tools.get_machine_storage()
@@ -9,19 +10,50 @@ MEMORY_AVAILABLE = tools.get_machine_storage()
 def index():
     return render_template('index.html', temperature_value = TEMPERATURE, memory_available_value = MEMORY_AVAILABLE)
 
+@app.route('/logout')
+def logout():
+    del session['email']
+    return redirect(url_for('index'))
+
+
+# if there's no email in the session is gonna force the user to login
+@app.before_request
+def require_login():
+    allowed_routes = ['login']
+    if request.endpoint not in allowed_routes and 'email' not in session:
+        return redirect(url_for('login'))
+
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
 
     if form.validate_on_submit():
         email = str(form.email.data)
-        password = tools.encrypt(str(form.password.data))
+        password = (str(form.password.data))
         print("email:  {}, password: {}".format(email, password))
-        print('SUCCESS')
-        return redirect(url_for('index'))
 
+        # rootAccount = User(email='test@root.com', password=tools.encrypt('secure-pi'))
+        # db.session.add(rootAccount)
+        # db.session.commit()
 
         # CHECK DB FOR ACCOUNT
+        query = User.query.filter_by(email=email).first()
+        if query:
+            print('email found')
+            if tools.check_hash(form.password.data, query.password):
+                print('password match with email')
+
+                session['email'] = form.email.data
+
+
+                return redirect(url_for('index'))
+            else:
+                print('doesn\'t match')
+                form.email.errors.append('Invalid account')
+        else:
+            print('couldn\'t find email address')
+            form.email.errors.append('Invalid account')
+
     else:
         print("FAIL")
     return render_template('login.html', form=form)
