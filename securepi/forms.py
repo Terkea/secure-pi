ac258105da019bdc994d7dbbb1e436b2e6ee2b86
Terkea <terkeabt@gmail.com>
2019-09-26 18:51:14 +0100

    notification settings almost done, still have some things to sort out on the update functionality

--- securepi/forms.py
+++ securepi/forms.py
@@ -6,8 +6,11 @@ class LoginForm(FlaskForm):
     email = StringField('Email Address', validators=[DataRequired(message='Field required'), Email()])
     password = PasswordField('Password', validators=[DataRequired(message='Field required')])
 
-class UpdateEmailForm(FlaskForm):
-        server = StringField('Server', validators=[DataRequired(message='Field required')])
-        email = StringField('Email Address', validators=[DataRequired(message='Field required'), Email()])
-        password = PasswordField('Password', validators=[DataRequired(message='Field required')])
-        port = StringField('Port', validators=[DataRequired(message='Field required')])
+class UpdateSMTPForm(FlaskForm):
+    server = StringField('Server', validators=[DataRequired(message='Field required')])
+    email = StringField('Email Address', validators=[DataRequired(message='Field required'), Email()])
+    password = PasswordField('Password', validators=[DataRequired(message='Field required')])
+    port = StringField('Port', validators=[DataRequired(message='Field required')])
+
+class UpdateEmailAddress(FlaskForm):
+    email = StringField('Email Address', validators=[DataRequired(message='Field required'), Email()])
--- securepi/routes.py
+++ securepi/routes.py
@@ -1,6 +1,6 @@
 from flask import Flask, escape, request, render_template, redirect, url_for, session
-from securepi import app, tools
-from securepi.forms import LoginForm, UpdateEmailForm
+from securepi import app, tools, db
+from securepi.forms import LoginForm, UpdateSMTPForm, UpdateEmailAddress
 from securepi.models import User, Email, Picture, WhiteList
 import json
 
@@ -39,10 +39,6 @@ def login():
         password = (str(form.password.data))
         print("email:  {}, password: {}".format(email, password))
 
-        # rootAccount = User(email='test@root.com', password=tools.encrypt('secure-pi'))
-        # db.session.add(rootAccount)
-        # db.session.commit()
-
         # CHECK DB FOR ACCOUNT
         query = User.query.filter_by(email=email).first()
         if query:
@@ -64,7 +60,8 @@ def login():
 
 @app.route('/smtp/', methods=['GET', 'POST'])
 def smtp():
-    form = UpdateEmailForm()
+    form = UpdateSMTPForm()
+    form2 = UpdateEmailAddress()
     query = Email.query.all()
 
     if  form.validate_on_submit():
@@ -85,5 +82,53 @@ def smtp():
         else:
             form.server.errors.append("Invalid account, be sure that you have less secure app access turned on or try with a gmail account")
 
-    # print(json.dumps(CONFIG['SMTP']))
-    return render_template('smtp.html', config=CONFIG['SMTP'], query=query, form=form)
+
+    if form2.validate_on_submit():
+        print('form2 email updated {}'.format(form2.email))
+    else:
+        print('cannot validate the second form')
+
+    return render_template('smtp.html', config=CONFIG['SMTP'], query=query, form=form, form2=form2)
+
+@app.route('/delete_email/<int:id>', methods=['GET', 'POST'])
+def delete_email(id):
+    query = Email.query.get_or_404(id)
+
+    try:
+        db.session.delete(query)
+        db.session.commit()
+        return redirect(url_for('smtp'))
+    except:
+        return 'There was a problem deleting that email'
+
+@app.route('/change_notification_status/<int:id>', methods=['GET', 'POST'])
+def change_notification_status(id):
+    query = Email.query.get_or_404(id)
+
+    try:
+        if query.notifications == True:
+            query.notifications = False
+            db.session.commit()
+            return redirect(url_for('smtp'))
+        else:
+            query.notifications = True
+            db.session.commit()
+            return redirect(url_for('smtp'))
+    except:
+        return 'There was a problem changing the notification status on that email'
+
+@app.route('/update_email_address/<int:id>', methods=['GET', 'POST'])
+def update_email_address(id):
+    query = Email.query.get_or_404(id)
+    form2 = UpdateEmailAddress()
+
+    if form2.validate_on_submit():
+        try:
+            print(form2.email)
+            query.email = form2.email.update_email_address
+            db.session.commit()
+        except:
+            return 'There was a problem changing the notification status on that email'
+    else:
+        print('couldn\'t validate the form')
+    return render_template(url_for('smtp'))
