7828492d98bdafcb7fff98b6ecd552f7dbbcddca
Terkea <terkeabt@gmail.com>
2019-09-27 01:04:41 +0100

    settings page -- SYSTEM half way done

--- securepi/routes.py
+++ securepi/routes.py
@@ -132,4 +132,5 @@ def change_notification_status(id):
 
 @app.route('/settings', methods=['GET', 'POST'])
 def settings():
+    # todo gotta check the configuration before rendering the view
     return render_template('settings.html', config=CONFIG["NETWORK"], temperature_value = TEMPERATURE, memory_available_value = MEMORY_AVAILABLE)
